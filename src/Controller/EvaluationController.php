004bf0d006dfb49a6ec1f7ef47b29904c14b24db
thibaultfradet <fradet.thibault23@gmail.com>
2025-09-25 08:29:47 +0200

    Evaluation student + changing student remarks to contents

--- src/Controller/EvaluationController.php
+++ src/Controller/EvaluationController.php
@@ -9,6 +9,7 @@ use App\Entity\TutorEvaluation;
 use App\Entity\TutorEvaluationBehavior;
 use App\Entity\TutorEvaluationSkill;
 use App\Entity\User;
+use App\Form\StudentEvaluationFormType;
 use App\Form\TutorEvaluationFormType;
 use App\Repository\BehaviorCriteriaRepository;
 use App\Repository\BehaviorLevelRepository;
@@ -18,6 +19,7 @@ use App\Repository\SkillLevelRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -105,137 +107,38 @@ final class EvaluationController extends AbstractController
     }
 
 
+    #[Route('/evaluation/student/{period}', name: 'app_create_student_evaluation')]
+    public function student(
+        Period $period,
+        Request $request,
+        EntityManagerInterface $em,
+    ): Response {
+        // set data based on context
+        $evaluation = new StudentEvaluation();
+        $student = $this->getUser();
 
+        $evaluation->setStudent($student);
+        $evaluation->setPeriod($period);
 
-    // #[Route('/evaluation/tuteur/{id}', name: 'app_evaluation_tuteur')]
-    // public function tuteur(int $id, Request $request, UserRepository $userRepo, SkillCriteriaRepository $SCRepo, SkillLevelRepository $SLRepo, BehaviorCriteriaRepository $BCRepo, BehaviorLevelRepository $BLRepo): Response
-    // {
+        // create form
+        $form = $this->createForm(StudentEvaluationFormType::class, $evaluation);
+        $form->handleRequest($request);
+        dump("test");
+        // handle submit
+        if ($form->isSubmitted() && $form->isValid()) {
 
-    //     $this->denyAccessUnlessGranted('ROLE_TUTOR');
-
-
-    //     if ($request->isMethod('POST')) {
-    //     }
-
-    //     //get all skills and behavior and level
-    //     //warning : we only want the attached skill of the class level
-
-    //     $user = $this->getUser();
-
-    //     $critere_competence = $SCRepo->createQueryBuilder('c')
-    //         ->join('c.titresCompetence', 't')
-    //         ->join('t.classe', 'cl')
-    //         ->where('cl = :classe')
-    //         ->setParameter('classe', $user->getClassroom())
-    //         ->getQuery()
-    //         ->getResult();
-
-
-    //     $critere_comportement = $BCRepo->findAll();
-    //     $niveau_comportement =  $BLRepo->findAll();
-
-    //     // get the concerned student
-    //     $student = $userRepo->findOneBy(['id' => $id]);
-
-    //     return $this->render('evaluation/tuteur.html.twig', [
-    //         'controller_name' => 'evaluationController',
-    //         'critere_competence' => $critere_competence,
-    //         'critere_comportement' => $critere_comportement,
-    //         'niveau_comportement' => $niveau_comportement,
-    //         'student' => $student,
-    //     ]);
-    // }
-
-
-    #[Route('/evaluation/student', name: 'app_evaluation_student')]
-    public function student(Request $request, PeriodRepository $periodRepo, EntityManagerInterface $em): Response
-    {
-
-        $this->denyAccessUnlessGranted('ROLE_STUDENT');
-
-        if ($request->isMethod('POST')) {
-
-            $user = $this->getUser();
-
-            //can be replace with a selector on periode on the before page? 
-            $now = date('Y-m-d');
-            $periode = $periodRepo->createQueryBuilder('p')
-                ->where('p.id_classe = :classe')
-                ->andWhere('p.date_debut <= :now')
-                ->andWhere('p.date_fin >= :now')
-                ->setParameter('classe', $user->getClasse()->getId())
-                ->setParameter('now', new \DateTime())
-                ->getQuery()
-                ->getOneOrNullResult();
-
-
-            // get data and create item
-            $remarque = $request->query->get('remarque');
-
-            $user = $this->getUser();
-            $now = new \DateTime();
-
-            $evaluation = new StudentEvaluation();
-            $evaluation->setPeriod($periode);
-            $evaluation->setStudent($user);
-            $evaluation->setRemarks($remarque);
-
-
-            // Persister et flush
             $em->persist($evaluation);
             $em->flush();
+
+            $this->addFlash('success', 'Votre auto-évaluation a été enregistrée avec succès !');
+
+            return $this->redirectToRoute('app_home');
         }
 
         return $this->render('evaluation/student.html.twig', [
-            'controller_name' => 'evaluationController',
-        ]);
-    }
-
-    #[Route('/evaluation/ttm/{id}', name: 'app_evaluation_ttm')]
-    public function ttm(int $id, Request $request, UserRepository $userRepo, PeriodRepository $periodeRepo, EntityManagerInterface $em): Response
-    {
-        // only ttm
-        $this->denyAccessUnlessGranted('ROLE_TTM');
-
-        if ($request->isMethod('POST')) {
-
-            $user = $this->getUser();
-
-            $now = date('Y-m-d');
-            $periode = $periodeRepo->createQueryBuilder('p')
-                ->where('p.id_classe = :classe')
-                ->andWhere('p.date_debut <= :now')
-                ->andWhere('p.date_fin >= :now')
-                ->setParameter('classe', $user->getClasse()->getId())
-                ->setParameter('now', new \DateTime())
-                ->getQuery()
-                ->getOneOrNullResult();
-
-
-            $student = $userRepo->findOneBy(['id' => $request->query->get('student')]);
-
-
-            // get data and create item
-            $remarque = $request->query->get('remarque');
-
-            $now = new \DateTime();
-
-            $evaluation = new TTMEvaluation();
-            $evaluation->setPeriod($periode);
-            $evaluation->setStudent($student);
-            $evaluation->setRemarks($remarque);
-
-
-            // Persister et flush
-            $em->persist($evaluation);
-            $em->flush();
-        }
-
-        $student = $userRepo->findOneBy(['id' => $id]);
-
-        return $this->render('evaluation/ttm.html.twig', [
-            'controller_name' => 'evaluationController',
+            'form' => $form->createView(),
             'student' => $student,
+            'period' => $period,
         ]);
     }
 }
---
+++ src/Form/StudentEvaluationFormType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\StudentEvaluation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class StudentEvaluationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('remarks', TextareaType::class, [
+                'label' => 'Remarque',
+                'attr' => [
+                    'class' => 'form-control',
+                    'rows' => 10,
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => StudentEvaluation::class,
+        ]);
+    }
+}
