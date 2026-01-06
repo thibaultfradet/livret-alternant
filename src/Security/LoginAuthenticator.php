21a9ba3f2fe28f742af8e66c35bb13eca3626034
thibaultfradet <fradet.thibault23@gmail.com>
2026-01-06 09:31:21 +0100

    feat(security): prevent login for users with disabledAt set

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
@@ -32,7 +33,21 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
         $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $email);
 
         return new Passport(
-            new UserBadge($email),
+            new UserBadge($email, function($userIdentifier) {
+                $user = $this->entityManager->getRepository(\App\Entity\User::class)
+                    ->findOneBy(['email' => $userIdentifier]);
+
+                if (!$user) {
+                    throw new CustomUserMessageAuthenticationException('Connexion impossible.');
+                }
+
+                if ($user->getDisabledAt() !== null) {
+                    // This prevents authentication and shows this message to the user
+                    throw new CustomUserMessageAuthenticationException('Connexion refusée.');
+                }
+
+                return $user;
+            }),
             new PasswordCredentials($request->getPayload()->getString('password')),
             [
                 new CsrfTokenBadge('authenticate', $request->getPayload()->getString('_csrf_token')),
