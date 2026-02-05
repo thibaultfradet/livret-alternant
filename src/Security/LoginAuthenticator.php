875825e3bf1a5f86e3e2372bee01893d4585ee7a
thibaultfradet <fradet.thibault23@gmail.com>
2026-02-05 11:04:49 +0100

    chore(auth): try forcing https redirect in authenticator

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -58,15 +58,28 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
-            return new RedirectResponse($targetPath);
+            return new RedirectResponse($this->forceHttps($targetPath));
         }
 
-        // For example:
-        return new RedirectResponse($this->urlGenerator->generate('app_home'));
+        return new RedirectResponse(
+            $this->forceHttps(
+                $this->urlGenerator->generate('app_home')
+            )
+        );
     }
 
     protected function getLoginUrl(Request $request): string
     {
         return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+    }
+
+
+    private function forceHttps(string $url): string
+    {
+        if (str_starts_with($url, 'http://')) {
+            return 'https://' . substr($url, 7);
+        }
+
+        return $url;
     }
 }
